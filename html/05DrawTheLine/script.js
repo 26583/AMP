2d6a423af9ce88827a5c47a9744746f20c7d45fa
26583 <you@example.com26583@ma-web.nl>
2019-09-27 13:49:23 +0200

    linear

--- html/05DrawTheLine/script.js
+++ html/05DrawTheLine/script.js
@@ -8,42 +8,8 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-let blueCs = 0;
-let mouse = new Vector2d(0,0);
-let points = [];
-for(let i = 0; i<3;i++){
-  let  point = new Point(new Vector2d(getRandom(width),getRandom(height)), 20, 255,0,0,true);
-  points.push(point);
+let f = new LinearFunction(0.6,200);
+for (var x = 0; x < width; x+=10) {
+  let point = new Point(new Vector2d(x,f.calcY(x)),10,0,0,255,false);
+  point.draw(context);
 }
-function animate(){
-  window.requestAnimationFrame(animate);
-  context.clearRect(0,0,width,height);
-  context.fillStyle = "#fff5c6";
-    context.beginPath();
-    context.moveTo(points[0].position.dx,points[0].position.dy);
-    context.lineTo(points[1].position.dx,points[1].position.dy);
-    context.lineTo(points[2].position.dx,points[2].position.dy);
-    context.fill();
-    for(let i = 0; i<points.length;i++){
-      points[i].draw(context);
-    }
-
-}
-window.addEventListener("mousemove",(evt)=>{
-  let anyHover = false;
-  let mouse = new Vector2d(evt.clientX,evt.clientY);
-  let d = new Vector2d(0,0);
-  for (var i = 0; i < points.length; i++) {
-    d.differenceVector(mouse,points[i].position);
-    if(d.magnitude<points[i].size){
-      anyHover = true;
-    }
-  }
-  if(!anyHover){
-    document.body.style.cursor = "auto";
-  }
-});
-function getRandom(max) {
-  return Math.floor((Math.random() * max) + 1);
-}
-animate();
